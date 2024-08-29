dccc24c858bc2633946cd97b4327b4a67076119f
naim195 <119221365+naim195@users.noreply.github.com>
2024-08-29 18:43:23 +0530

    changed the order of graphs

--- client/src/components/Graphs.jsx
+++ client/src/components/Graphs.jsx
@@ -1,6 +1,32 @@
 const Graphs = () => {
   return (
     <div className="container mx-auto p-4 max-w grid grid-cols-1 md:grid-cols-2 gap-8">
+      
+
+      {/* Power Flow GIF */}
+      <div className="flex flex-col items-center justify-center bg-gradient-to-r from-purple-100 to-pink-100 rounded-lg shadow-md p-6 hover:shadow-lg transition-shadow duration-300">
+        <h2 className="text-2xl font-bold mb-4 text-center text-purple-800">
+          Power Flow
+        </h2>
+        <div className="flex justify-center mb-6">
+          <img
+            id="gif3"
+            src="/assets/load_animation_combined.gif"
+            alt="Power Flow Animation"
+          />
+        </div>
+        <div className="description text-gray-800 text-center max-w-3xl">
+          <p className="text-sm">
+            This animation provides insights into the power flow from each
+            component in the system at different time intervals. It compares
+            unoptimized (left) and optimized (right) power flows. Negative power
+            flow indicates power being sold to the grid or used to charge the
+            battery. The difference between the two plots is based on the cost
+            associated with the power.
+          </p>
+        </div>
+      </div>
+
       {/* Energy Consumption and Production Images */}
       <div className="flex flex-col items-center justify-center bg-gradient-to-r from-blue-100 to-green-100 rounded-lg shadow-md p-6 hover:shadow-lg transition-shadow duration-300 w-full">
         <h2 className="text-2xl font-bold mb-4 text-center text-blue-800">
@@ -31,30 +57,6 @@ const Graphs = () => {
             component depending on the associated cost. The difference between
             the two operations is the energy delivered to the grid, representing
             the profit made by the system.
-          </p>
-        </div>
-      </div>
-
-      {/* Power Flow GIF */}
-      <div className="flex flex-col items-center justify-center bg-gradient-to-r from-purple-100 to-pink-100 rounded-lg shadow-md p-6 hover:shadow-lg transition-shadow duration-300">
-        <h2 className="text-2xl font-bold mb-4 text-center text-purple-800">
-          Power Flow
-        </h2>
-        <div className="flex justify-center mb-6">
-          <img
-            id="gif3"
-            src="/assets/load_animation_combined.gif"
-            alt="Power Flow Animation"
-          />
-        </div>
-        <div className="description text-gray-800 text-center max-w-3xl">
-          <p className="text-sm">
-            This animation provides insights into the power flow from each
-            component in the system at different time intervals. It compares
-            unoptimized (left) and optimized (right) power flows. Negative power
-            flow indicates power being sold to the grid or used to charge the
-            battery. The difference between the two plots is based on the cost
-            associated with the power.
           </p>
         </div>
       </div>
